0bc1bf3fd7edb669a9c0cc88d32e36102999d817
daler3 <daniroma3@gmail.com>
2016-05-17 01:26:26 +0200

    Added Csv Reader Class

--- CityBike/src/primo/CsvReaderBike.java
+++ CityBike/src/primo/CsvReaderBike.java
@@ -8,36 +8,38 @@ import com.csvreader.*;
 
 public class CsvReaderBike {
 	
-	 try {
-
-         CsvReader products = new CsvReader("products.csv");
-
-         products.readHeaders();
-
-         while (products.readRecord())
-         {
-             String productID = products.get("ProductID");
-             String productName = products.get("ProductName");
-             String supplierID = products.get("SupplierID");
-             String categoryID = products.get("CategoryID");
-             String quantityPerUnit = products.get("QuantityPerUnit");
-             String unitPrice = products.get("UnitPrice");
-             String unitsInStock = products.get("UnitsInStock");
-             String unitsOnOrder = products.get("UnitsOnOrder");
-             String reorderLevel = products.get("ReorderLevel");
-             String discontinued = products.get("Discontinued");
-
-             // perform program logic here
-             System.out.println(productID + ":" + productName);
-         }
-
-         products.close();
-
-     } catch (FileNotFoundException e) {
-         e.printStackTrace();
-     } catch (IOException i) {
-         e.printStackTrace();
-     }
+	public void getField(){
+		 try {
+	
+	         CsvReader products = new CsvReader("products.csv");
+	
+	         products.readHeaders();
+	
+	         while (products.readRecord())
+	         {
+	             String productID = products.get("ProductID");
+	             String productName = products.get("ProductName");
+	             String supplierID = products.get("SupplierID");
+	             String categoryID = products.get("CategoryID");
+	             String quantityPerUnit = products.get("QuantityPerUnit");
+	             String unitPrice = products.get("UnitPrice");
+	             String unitsInStock = products.get("UnitsInStock");
+	             String unitsOnOrder = products.get("UnitsOnOrder");
+	             String reorderLevel = products.get("ReorderLevel");
+	             String discontinued = products.get("Discontinued");
+	
+	             // perform program logic here
+	             System.out.println(productID + ":" + productName);
+	         }
+	
+	         products.close();
+	
+	     } catch (FileNotFoundException e) {
+	         e.printStackTrace();
+	     } catch (IOException i) {
+	         i.printStackTrace();
+	     }
+	}
 	
 
 }
